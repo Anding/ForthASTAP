c756412602e58031781dba5d52a135698b084835
Andrew Read <andrew81244@outlook.com>
2025-12-20 15:06:07 +0800

    coded ASTAP.readINI

--- ForthASTAP.f
+++ ForthASTAP.f
@@ -1,36 +1,44 @@
 need finiteFractions
 
-256 buffer: ForthASTAP.linebuffer
+256 buffer: ASTAP.linebuffer
 	
 \ compute a hash h1 by hashing x1 and h0
-: ForthASTAP.hash ( x1 h0 -- h1)
+: ASTAP.hash ( x1 h0 -- h1)
 	31 * swap 13 + xor
 ;	
 	
 \ hash a string to a single value on stack
 \ 	borrowed from simple-tester
-: ForthASTAP.hash$ ( c-addr u -- h)
-	swap 2dup + 1- swap ( u end start)
-		?do												\ Let h0 = u
-			i c@ ( h_i x) swap hash ( h_j)		\ j = i + 1
+: ASTAP.hash$ ( c-addr u -- h)
+	swap 2dup + swap ( u end+1 start)
+		?do													\ Let h0 = u
+			i c@ ( h_i x) swap ASTAP.hash ( h_j) 	\ j = i + 1
 		loop
 ;
 
-\ Invoke ASTAP Astrometry Stacking Program to plate solve an image
+
+\ Invoke ASTAP Astrometry Stacking Program to plate solinclude ve an image
 \ 	take the full file path of the image 
 \ 	return the RA and DEC as single integer finite fractions or an IOR on failure
-: platesolve ( " filepath.xisf" -- RA DEC 0 | IOR)
-	read-file open-file ( file-id) >R
-	ForthASTAP.linebuffer dup 256 R@ ( c-addr c-addr u1 fileid) read-line ( c-addr u2 flag ior) 
-	drop if
-		2dup drop 5 hash$ 
+: ASTAP.readINI { caddr u | ra dec flag -- RA DEC 0  | IOR }
+	caddr u r/o open-file ( file-id IOR ) ?dup if exit then >R
+	0 -> flag	
+	begin
+		ASTAP.linebuffer dup 256 R@ ( c-addr c-addr u1 fileid) read-line ( c-addr u2 flag ior) drop
+	while
+		2dup drop 6 ASTAP.hash$ ( c-addr u2 h)
 		case
-		 1234 ( "CRVAL1") of 
-		 	endof
-		 5678 ( "CRVAL2") of 
-		 	endof
-		 endcase
-	end if
+		-1959004665 ( "CRVAL1") of 7 /string >float drop fp~ -> ra -1 -> flag endof
+		-1959004666 ( "CRVAL2") of 7 /string >float drop fp~ -> dec endof
+		nip nip 
+		endcase
+	repeat
+	drop drop 	
+	R> close-file drop	
+	flag if ra dec 0 else -1 then
 ;
 		
-	
+: test
+	s" E:\coding\ForthASTAP\Resources\image1.ini"
+	ASTAP.readINI
+;
