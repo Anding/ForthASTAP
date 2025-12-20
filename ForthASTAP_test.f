4e2e04484d115c12618c4ef937cc7d7a43fffd20
Andrew Read <andrew81244@outlook.com>
2025-12-20 15:43:22 +0800

    updated testcase

--- ForthASTAP_test.f
+++ ForthASTAP_test.f
@@ -5,5 +5,7 @@ CR
 Tstart
 t{ s" E:\coding\ForthASTAP\Resources\image1.ini" ASTAP.readINI }T 13625 86003 0 ==
 t{ s" E:\coding\ForthASTAP\Resources\image2.ini" ASTAP.readINI }T 19577 59779 0 ==
+t{ s" E:\coding\ForthASTAP\Resources\image3.ini" ASTAP.readINI }T 19577 -59779 0 ==
+t{ s" E:\coding\ForthASTAP\Resources\image4.ini" ASTAP.readINI }T -1 ==
 Tend
 CR
